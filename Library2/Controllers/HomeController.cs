8452d48d354df8c7c269c2c0c91e2a85dbaab3d8
bsk <bsk@DESKTOP-HKUS83K>
2017-09-28 22:52:19 +0300

    Add project files.

---
+++ Library2/Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Library2.Models;
+
+namespace Library2.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        BOOKSTOREEntities db = new BOOKSTOREEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(FormCollection form)
+        {
+
+            Books model = new Books();
+            Publishers model1 = new Publishers();
+
+            model.BookName = form["bookName"];
+            model.Writer = form["writer"];
+            model.PublisherName = form["publisher"];
+            model1.PublisherName = form["publisher"];
+            model.PublishDate = Convert.ToDateTime(form["datetimepicker1"]);
+
+            db.Books.Add(model);
+            db.Publishers.Add(model1);
+            db.SaveChanges();
+
+
+
+            return View();
+        }
+
+        public ActionResult Update()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult List()
+        {
+           
+
+            return View(db.Books.ToList());
+        }
+
+        public ActionResult Delete()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Library2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Library2.Startup))]
+namespace Library2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
